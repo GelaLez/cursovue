8e2ddaaf553e95fe2398c10b94d3a82d0ea00a22
GelaLinux <gelagelasonic2@hotmail.com>
2023-04-04 12:13:52 -0600

    importacion de componentes

---
+++ src/components/PostCard.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+// importar el component postbutton
+import PostButton from './PostButton.vue';
+
+</script>
+
+
+<template>
+    <div class="post-card">
+        <h3>titulo</h3>
+        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam, quasi quod eius, inventore suscipit modi, earum
+            commodi distinctio repellendus ea quae? Inventore aliquam libero iure ab laborum provident eum at? </p>
+        <PostButton />
+    </div>
+</template>
+
+
+
+<style scoped>
+.post-card {
+    border-radius: 15px;
+    padding: 10px;
+    margin: 5px;
+    border: 1px solid #ccc;
+}
+</style>
