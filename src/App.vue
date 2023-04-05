f20f7379a60b1b618948e7f59efdf7347d5ec678
GelaLinux <gelagelasonic2@hotmail.com>
2023-04-04 18:57:25 -0600

    direcctiva v-for

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,14 @@
 <script lang="ts" setup>
- import PostContainer  from './components/PostContainer.vue'
- import CounterOption from './components/CounterOption.vue';
- import CounterComposition from './components/CounterComposition.vue';
- import ExampleSelective from './components/ExampleSelective.vue';
+import PostContainer from './components/PostContainer.vue'
+import CounterOption from './components/CounterOption.vue';
+import CounterComposition from './components/CounterComposition.vue';
+import ExampleSelective from './components/ExampleSelective.vue';
+import ExampleFor from './components/ExampleFor.vue';
 </script>
 
 <template>
-        <ExampleSelective/>
+    <ExampleFor />
 </template>
-
-
-
 
 <style>
 h1 {
---
+++ src/components/ExampleFor.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul>
+        <li v-for="country in countries" v-bind:key="country">
+            {{ country.name }}
+        </li>
+    </ul>
+</template>
+
+
+<script lang="ts" setup>
+
+import { Ref, ref } from 'vue'
+
+// array simple
+// let countries: Ref<Array<string>> = ref([
+//     'Spain', 'France', 'Uk', 'Italy', 'Portugal'
+// ])
+
+
+interface ICountry {
+    code: string,
+    name: string
+}
+
+let countries: Ref<Array <ICountry>> = ref([
+    {
+        code: 'es',
+        name: 'Spain'
+    },
+    {
+        code: 'en',
+        name: 'UK'
+    }
+])
+
+</script>
+
+<style scoped>
+h1 {
+    color: royalblue
+}
+</style>
