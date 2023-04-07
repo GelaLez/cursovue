e2bd7f5467763fe0034581e003a8cab50d2cb04f
GelaLinux <gelagelasonic2@hotmail.com>
2023-04-06 18:58:04 -0600

    modificado

--- src/components/ModelForm.vue
+++ src/components/ModelForm.vue
@@ -2,6 +2,12 @@
     hellow gela
     <form>
         <input type="text" v-model="inputText">
+        <input type="checkbox" v-model="agree" />Acepto los terminos y condiciones
+        <select v-model="country">
+            <option>Spain</option>
+            <option>Uk</option>
+            <option>France</option>
+        </select>
     </form>
     <h3>{{ inputText }}</h3>
 </template>
@@ -11,6 +17,8 @@
 <script lang="ts" setup>
 import { Ref, ref } from 'vue'
 let inputText: Ref<string> = ref()
+let agree = ref(true)
+let country = ref('Spain')
 
 </script>
 
